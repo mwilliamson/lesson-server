1eb889395e69ae67c6b58aa502c2d846ea7c35a7
Michael Williamson <mike@zwobble.org>
2014-06-30 19:39:13 +0100

    Simplify running of lesson JavaScript

---
+++ browser/lesson.js
@@ -0,0 +1,6 @@
+var lessonContainers = document.querySelectorAll("*[data-lesson-module]");
+Array.prototype.forEach.call(lessonContainers,  function(lessonContainer) {
+    var module = window[lessonContainer.getAttribute("data-lesson-module")];
+    var contentElement = lessonContainer.getElementsByClassName("content-container")[0];
+    module.init(contentElement);
+});
