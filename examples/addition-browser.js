ec5ce0601962c1f49503969ce186e4da33f0e9ee
Michael Williamson <mike@zwobble.org>
2014-06-30 18:15:19 +0100

    Run browser-side JavaScript on lesson

---
+++ examples/addition-browser.js
@@ -0,0 +1,6 @@
+module.exports = {
+    init: function(element) {
+        element.innerHTML += "<p>4 + 5 = 9</p>";
+    }
+};
+
--- examples/addition.js
+++ examples/addition.js
@@ -3,5 +3,6 @@ var fs = require("fs");
 module.exports = {
     uid: "a242dcab-aa22-4464-a83a-f4bda1b19767",
     title: "Addition",
-    lessonHtml: fs.readFileSync(__dirname + "/addition.html")
+    lessonHtml: fs.readFileSync(__dirname + "/addition.html"),
+    browserModule: __dirname + "/addition-browser.js"
 };
